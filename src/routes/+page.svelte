45e4525206d10c90748294c96bbac2c8008e255e
Phantomlsh <phantomlsh@outlook.com>
2022-12-23 18:58:02 -0800

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<svelte:head>
+  <title>ITI</title>
+</svelte:head>
+
+<script>
+let cot = 0
+</script>
+
+<div class="p-2">
+  <div>Hello, world!</div>
+  <div>cot = { cot }</div>
+  <button on:click={() => cot++}>Click me!</button>
+</div>
